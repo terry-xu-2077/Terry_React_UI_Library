86fa498620cf24b17378c9be51a67602d2efbff6
Terry <49803399+terry-xu-2077@users.noreply.github.com>
2026-09-04 09:48:01 +0800

    ui: make visual multi-select the shared MultiSelect export

--- src/styles/legacy-classic/index.ts
+++ src/styles/legacy-classic/index.ts
@@ -1,3 +1,5 @@
-export * from "./components";
-export * from "./visual-multi-select";
+export { Tooltip, ResetButton, TextField, BoolSwitch, Select, Slider, EntityHeader, Dialog, Button, StatusPill } from "./components";
+export type { OptionItem, AccentTone, DialogSize, EntityHeaderProps } from "./components";
+export { VisualMultiSelect, VisualMultiSelect as MultiSelect } from "./visual-multi-select";
+export type { VisualOptionItem, VisualMultiSelectProps, VisualMultiSelectProps as MultiSelectProps, VisualMultiSelectMode, VisualMultiSelectMode as MultiSelectMode, VisualOptionIconDescriptor } from "./visual-multi-select";
 export * from "./showcase";
